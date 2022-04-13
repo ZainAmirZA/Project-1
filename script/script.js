6d0afaf0aa584b9726af2607e98cd71ce1c25dc6
zainamirza <zain_amir@outlook.com>
2022-04-13 18:02:33 -0400

    Final formatting commit

--- script/script.js
+++ script/script.js
@@ -1,8 +1,5 @@
-// console.log("This is set up")
-
-
-const X_CLASS = 'x'
-const O_CLASS = 'o'
+const X_CLASS = "x";
+const O_CLASS = "o";
 const WINNING_COMBINATIONS = [
   [0, 1, 2],
   [3, 4, 5],
@@ -84,7 +81,7 @@ const WINNING_COMBINATIONS = [
   [74, 77, 80],
   [72, 76, 80],
   [74, 76, 78],
-]
+];
 
 const WINNING_FINALS = [
   [0, 1, 2],
@@ -95,143 +92,130 @@ const WINNING_FINALS = [
   [2, 5, 8],
   [0, 4, 8],
   [2, 4, 6],
-]
-const cellElements = document.querySelectorAll('[data-cell]')
-const bigCellElements = document.querySelectorAll('[data-big]')
-const board = document.getElementById('board')
-const winScreenElement = document.getElementById('winScreen')
-const restartButton = document.getElementById('restartButton')
-const winScreenTextElement = document.querySelector('[data-win-screen-text]')
-let oTurn = true
+];
+const cellElements = document.querySelectorAll("[data-cell]");
+const bigCellElements = document.querySelectorAll("[data-big]");
+const board = document.getElementById("board");
+const winScreenElement = document.getElementById("winScreen");
+const restartButton = document.getElementById("restartButton");
+const winScreenTextElement = document.querySelector("[data-win-screen-text]");
+let oTurn = true;
 
-startGame()
+startGame();
 
 //Build restart into startGame
-restartButton.addEventListener('click', startGame)
+restartButton.addEventListener("click", startGame);
 
 //Remove previous marks, Only Allow a single click per cell, remove winners message
 function startGame() {
-  oTurn = false
-  document.querySelectorAll('.sub-board').forEach(n =>n.classList.remove('o','x','highlight'))
-  cellElements.forEach(cell => {
-    cell.classList.remove(X_CLASS)
-    cell.classList.remove(O_CLASS)
-    cell.classList.remove('disabled')
-    cell.removeEventListener('click', takeClick)
-    cell.addEventListener('click', takeClick, { once: true })
-  })
-  setBoardHoverClass()
-  winScreenElement.classList.remove('show') 
+  oTurn = false;
+  document
+    .querySelectorAll(".sub-board")
+    .forEach((n) => n.classList.remove("o", "x", "highlight"));
+  cellElements.forEach((cell) => {
+    cell.classList.remove(X_CLASS);
+    cell.classList.remove(O_CLASS);
+    cell.classList.remove("disabled");
+    cell.removeEventListener("click", takeClick);
+    cell.addEventListener("click", takeClick, { once: true });
+  });
+  setBoardHoverClass();
+  winScreenElement.classList.remove("show");
 }
-
-board.addEventListener('click', function(e){
-  let clicked = e.path
-  console.log(clicked)
-})
 
 
 // In Click must place a mark, check for a win, check for a draw, switch turns
 function takeClick(e) {
-  const cell = e.target
-  if (cell.classList.contains('disabled')) return
-  const bigCell = e.path[1]
-  // console.log(bigCell.childNodes)
-  const currentClass = oTurn ? O_CLASS : X_CLASS
-  placeMark(cell, currentClass)
-  
+  const cell = e.target;
+  if (cell.classList.contains("disabled")) return;
+  const bigCell = e.path[1];
+  const currentClass = oTurn ? O_CLASS : X_CLASS;
+  placeMark(cell, currentClass);
+
   if (checkWin(currentClass)) {
     //Remove all classes from cells in that  big cell
     bigCell.childNodes.forEach((childCell, idx) => {
-      
-      if (idx % 2 !== 0 ) {
-      childCell.classList.remove("x","o")
-      childCell.classList.add('disabled')
-      console.log(childCell)
+      if (idx % 2 !== 0) {
+        childCell.classList.remove("x", "o");
+        childCell.classList.add("disabled");
+        console.log(childCell);
       }
-      
-    }) 
+    });
 
-    placeBigMark(bigCell, currentClass)
-  } 
+    placeBigMark(bigCell, currentClass);
+  }
   if (checkBigWin(currentClass)) {
-    endGame(false)
+    endGame(false);
   } else if (isDraw()) {
-    endGame(true)
+    endGame(true);
   } else {
-    swapTurns()
-    setBoardHoverClass()
+    swapTurns();
+    setBoardHoverClass();
   }
 }
-
 
 //At the end of game show the win-screen or draw message - write whatt should be inserted in the winning message
 function endGame(draw) {
   if (draw) {
-    winScreenTextElement.innerText = 'Draw!'
+    winScreenTextElement.innerText = "Draw!";
   } else {
-    winScreenTextElement.innerText = `${oTurn ? "O" : "X"} Wins!`
+    winScreenTextElement.innerText = `${oTurn ? "O" : "X"} Wins!`;
   }
-  winScreenElement.classList.add('show')
+  winScreenElement.classList.add("show");
 }
 
-//Check if all cells are full with classes 
+//Check if all cells are full with classes
 function isDraw() {
-  return [...cellElements].every(cell => {
-    return cell.classList.contains('disabled')
-  })
+  return [...cellElements].every((cell) => {
+    return cell.classList.contains("disabled");
+  });
 }
 
 //Function to add the mark upon click aka add the class of X or O to the cell
 function placeMark(cell, currentClass) {
-  cell.classList.add(currentClass, 'disabled')
+  cell.classList.add(currentClass, "disabled");
 }
 
 //Function to add a big mark upon winning grid
 function placeBigMark(bigCell, currentClass) {
-  bigCell.classList.remove(X_CLASS)
-  bigCell.classList.remove(O_CLASS)
-  bigCell.classList.add(currentClass)
-  bigCell.classList.add('highlight')
+  bigCell.classList.remove(X_CLASS);
+  bigCell.classList.remove(O_CLASS);
+  bigCell.classList.add(currentClass);
+  bigCell.classList.add("highlight");
 }
 
-function checkSubWin(bigCell, currentClass) {
-   
-}
-
-//When called just swap 
+//When called just swap
 function swapTurns() {
-  oTurn = !oTurn
+  oTurn = !oTurn;
 }
 
 //ONLY CALL RIGHT AFTER SWAPTURNS
 //removes the class to add them in hover state
 function setBoardHoverClass() {
-  board.classList.remove(X_CLASS)
-  board.classList.remove(O_CLASS)
+  board.classList.remove(X_CLASS);
+  board.classList.remove(O_CLASS);
   if (oTurn) {
-    board.classList.add(O_CLASS)
+    board.classList.add(O_CLASS);
   } else {
-    board.classList.add(X_CLASS)
+    board.classList.add(X_CLASS);
   }
 }
 
 //Check if ANY of the winning combinations are met on the small board
 function checkWin(currentClass) {
-  return WINNING_COMBINATIONS.some(combination => {
-    return combination.every(index => {
-      return cellElements[index].classList.contains(currentClass)
-    })
-  })
+  return WINNING_COMBINATIONS.some((combination) => {
+    return combination.every((index) => {
+      return cellElements[index].classList.contains(currentClass);
+    });
+  });
 }
 
 //Check if the big boards fit the winning combinations
 function checkBigWin(currentClass) {
-  return WINNING_FINALS.some(combination => {
-    return combination.every(index => {
-      console.log(bigCellElements[index].querySelector(`.${currentClass}`))
-      return bigCellElements[index].querySelector(`.sub-board.${currentClass}`)
-      
-      // .classList.contains(currentClass)
-    })
-  })
+  return WINNING_FINALS.some((combination) => {
+    return combination.every((index) => {
+      console.log(bigCellElements[index].querySelector(`.${currentClass}`));
+      return bigCellElements[index].querySelector(`.sub-board.${currentClass}`);
+    });
+  });
 }
